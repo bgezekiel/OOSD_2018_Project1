77660ff36a8337106ba9a585897285c04b87e510
bgezekiel <44778576+bgezekiel@users.noreply.github.com>
2018-11-28 20:22:54 -0700

    Create okotoks.php

---
+++ okotoks.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <title>Okotoks Agents</title>
+</head>
+<body>
+    <?php
+        include("nav.php");   
+    ?>
+
+     <div class="jumbotron-okotoks jumbotron-fluid">
+      <div class="container2">
+        <h1 class="display-3" id="oko">Meet our Okotoks Team!</h1>
+      </div>
+    </div>
+    <?php
+        include("okotoksagency.php");
+        include("footer.php");
+    ?>
+
+</html>
